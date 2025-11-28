ca85ffc6dfea9d9ca4f42dcecb01f4f5355a11b3
calebvallsj <calebvallsj@gmail.com>
2025-11-28 21:54:39 +0000

    okey

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,8 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MOCK_RECIPES } from './mock-recipes';
+import { RecipeModel } from './models';
+
 
 @Component({
   selector: 'app-root',
@@ -9,12 +12,26 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('My Recipe Box');
+  protected readonly recipe = signal(MOCK_RECIPES[0]);
 
   protected selectSpaghetti(): void {
     console.log('Loading Spaghetti Carbonara');
+    this.recipe.set(MOCK_RECIPES[0]);
   }
 
   protected selectSalad(): void {
     console.log('Loading Caprese Salad');
+    this.recipe.set(MOCK_RECIPES[1]);
   }
+
+  protected readonly count = signal(0);
+
+  protected increase(): void{
+    this.count.update(currentValue => currentValue + 1);
+  }
+
+  protected decrease(): void{
+    this.count.update(currentValue => currentValue - 1);
+  }
+
 }
--- src/app/models.ts
+++ src/app/models.ts
@@ -1,4 +1,3 @@
-
 export interface Ingredient {
   name: string;
   quantity: number;
