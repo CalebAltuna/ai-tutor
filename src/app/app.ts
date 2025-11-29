3326da40cad339661289c008452f9da1a29e763d
web24cvalls <web24cvalls@imaltuna.com>
2025-11-29 07:16:55 +0000

    haciendo el ejercicio numero 4 de la fase 2 (creo)

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,18 +1,23 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { JsonPipe } from '@angular/common'; // Asegúrate de que esta línea esté
 import { RouterOutlet } from '@angular/router';
 import { MOCK_RECIPES } from './mock-recipes';
 import { RecipeModel } from './models';
-
-
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, JsonPipe], // Y asegúrate de que JsonPipe esté aquí
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('My Recipe Box');
   protected readonly recipe = signal(MOCK_RECIPES[0]);
+
+  protected readonly adjustedIngredients = computed(() =>{
+    this.count;
+    this.recipe;
+    return this.recipe().ingredients;
+  });
 
   protected selectSpaghetti(): void {
     console.log('Loading Spaghetti Carbonara');
